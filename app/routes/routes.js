c6150ea3d3367d733c1d79e83e81fad8469a009d
Debchakra Majumder <dmajumder4292@gmail.com>
2022-05-08 15:56:08 +0530

    Add New API for 32-bit passwords

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -18,6 +18,8 @@ const passport = require('../middleware/passport');
 const TeamsRoutes = require('./teams');
 const logger = require('../../lib/logger');
 const moment = require('moment');
+const AesUtil = require('../../lib/AesUtil');
+const CryptoJS = require('crypto-js');
 
 const { passportAuth } = passport;
 
@@ -105,6 +107,112 @@ module.exports = (Router, Service, App) => {
 
       // Process user data and answer API call
       const pass = App.services.Crypt.decryptText(req.body.password);
+      // 2-Factor Auth. Verification
+      const needsTfa = userData.secret_2FA && userData.secret_2FA.length > 0;
+      let tfaResult = true;
+      if (needsTfa) {
+        tfaResult = speakeasy.totp.verifyDelta({
+          secret: userData.secret_2FA,
+          token: req.body.tfa,
+          encoding: 'base32',
+          window: 2
+        });
+      }
+      if (!tfaResult) {
+        return res.status(400).send({ error: 'Wrong 2-factor auth code' });
+      }
+
+      if (pass === userData.password.toString() && tfaResult) {
+        // Successfull login
+        const internxtClient = req.headers['storx-client'];
+        const token = passport.Sign(userData.email, App.config.get('secrets').JWT, internxtClient === 'drive-web');
+
+        Service.User.LoginFailed(req.body.email, false);
+        Service.User.UpdateAccountActivity(req.body.email);
+        const userBucket = await Service.User.GetUserBucket(userData);
+
+        const keyExists = await Service.KeyServer.keysExists(userData);
+
+        if (!keyExists && req.body.publicKey) {
+          await Service.KeyServer.addKeysLogin(userData, req.body.publicKey, req.body.privateKey, req.body.revocateKey);
+        }
+
+        const keys = await Service.KeyServer.getKeys(userData);
+        const hasTeams = !!(await Service.Team.getTeamByMember(req.body.email));
+
+        const user = {
+          email: req.body.email,
+          userId: userData.userId,
+          mnemonic: userData.mnemonic,
+          root_folder_id: userData.root_folder_id,
+          name: userData.name,
+          lastname: userData.lastname,
+          uuid: userData.uuid,
+          credit: userData.credit,
+          createdAt: userData.createdAt,
+          privateKey: keys ? keys.private_key : null,
+          publicKey: keys ? keys.public_key : null,
+          revocateKey: keys ? keys.revocation_key : null,
+          bucket: userBucket,
+          registerCompleted: userData.registerCompleted,
+          teams: hasTeams
+        };
+
+        const userTeam = null;
+        if (userTeam) {
+          const tokenTeam = passport.Sign(userTeam.bridge_user, App.config.get('secrets').JWT, internxtClient === 'drive-web');
+          return res.status(200).json({
+            user, token, userTeam, tokenTeam
+          });
+        }
+        return res.status(200).json({ user, token, userTeam });
+      }
+      // Wrong password
+      if (pass !== userData.password.toString()) {
+        Service.User.LoginFailed(req.body.email, true);
+      }
+
+      return res.status(400).json({ error: 'Wrong email/password' });
+    }).catch((err) => {
+      Logger.error(`${err.message}\n${err.stack}`);
+      return res.status(400).send({ error: 'User not found on Cloud database', message: err.message });
+    });
+  });
+
+  Router.post('/access_core', (req, res) => {
+    const MAX_LOGIN_FAIL_ATTEMPTS = 5;
+
+    // Call user service to find or create user
+    Service.User.FindUserByEmail(req.body.email).then(async (userData) => {
+      if (userData.errorLoginCount >= MAX_LOGIN_FAIL_ATTEMPTS) {
+        return res.status(500).send({ error: 'Your account has been blocked for security reasons. Please reach out to us' });
+      }
+
+      if (userData.registerCompleted == false) {
+        return res.status(400).send({ error: 'Please verify your email' });
+      }
+      //Creating the Vector Key, this will come from env.REACT_APP_MAGIC_IV 
+      var iv = CryptoJS.enc.Hex.parse(process.env.MAGIC_IV);
+      //Encoding the Password in from UTF8 to byte array, this will we use env.APP_SEGMENT_KEY 
+      var Pass = CryptoJS.enc.Utf8.parse(process.env.APP_SEGMENT_KEY);
+      //Encoding the Salt in from UTF8 to byte array, this will come from env.MAGIC_SALT 
+      var Salt = CryptoJS.enc.Utf8.parse(process.env.MAGIC_SALT);
+      //Creating the key in PBKDF2 format to be used during the decryption
+      var key128Bits1000Iterations = CryptoJS.PBKDF2(Pass.toString(CryptoJS.enc.Utf8), Salt, { keySize: 128 / 32, iterations: 1000 });
+      //Enclosing the test to be decrypted in a CipherParams object as supported by the CryptoJS libarary, this will come from body.password
+      var cipherParams = CryptoJS.lib.CipherParams.create({
+          ciphertext: CryptoJS.enc.Hex.parse(req.body.password)
+      });
+
+      //Decrypting the string contained in cipherParams using the PBKDF2 key
+      var decrypted = CryptoJS.AES.decrypt(cipherParams, key128Bits1000Iterations, { mode: CryptoJS.mode.CBC, iv: iv, padding: CryptoJS.pad.Pkcs7 });
+      
+      const salt = AesUtil.decryptText(req.body.sKey);
+      const hashObj = AesUtil.passToHash({ password: decrypted.toString(CryptoJS.enc.Utf8), salt });
+      const encPass = AesUtil.encryptText(hashObj.hash);
+      // console.log("decrypted.toString(CryptoJS.enc.Utf8)",decrypted.toString(CryptoJS.enc.Utf8));
+      // Process user data and answer API call
+      const pass = App.services.Crypt.decryptText(encPass);
       // 2-Factor Auth. Verification
       const needsTfa = userData.secret_2FA && userData.secret_2FA.length > 0;
       let tfaResult = true;
